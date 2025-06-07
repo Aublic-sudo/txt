8d4b536227771316f234c1a3a92058a0d32490be
Aublic-sudo <rstock937@gmail.com>
2025-06-07 14:25:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    AUTH_USERS = os.environ.get("AUTH_USERS", "")
+    AUTH_USERS = os.environ.get("AUTH_USERS", "7360968885")
